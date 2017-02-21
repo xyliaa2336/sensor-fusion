d1c37c8826d722b4c6e31caf6fdd1e3aa78033d8
Xiaoya Li <xyliaa2336@gmail.com>
2017-02-21 20:50:16 +0100

    Update scripts for auto recording grid videos with DVS camera

--- data_collecting/audiovisual_acquire/auto_record_grid/auto_record_grid.m
+++ data_collecting/audiovisual_acquire/auto_record_grid/auto_record_grid.m
@@ -7,8 +7,8 @@
 %open udp port, u1 for cochlea, u2 for retina
 % u1=udp('localhost',8997); % make a UDP interface to localhost on port 8995 (the default port for AEViewer RemoteControl interface)
 % fopen(u1);
- %u2=udp('localhost',8997); % make a UDP interface to localhost on port 8997 (the default port for AEViewer RemoteControl interface)
-%fopen(u2)
+ u2=udp('localhost',8997); % make a UDP interface to localhost on port 8997 (the default port for AEViewer RemoteControl interface)
+fopen(u2)
 
 %set parameters for external microphone, if any
 % samp=48000
@@ -25,7 +25,7 @@ rec_cnt=0;
 %% recording loop
 fprintf('Start recording...\n')
 rootpath='D:\lipreading_raw_data\high_res\';
-dest_dir='D:\lipread_data\hw_grid_DVS_part2\'; % directory to save sensor files
+dest_dir='D:\lipread_data\hw_grid_DVS_part3\'; % directory to save sensor files
 %dest_dir='C:\Users\YY\polybox\thesis\lipreading\audiovisual_acquire\DVS_tests';
 
 % Create a cascade detector object.
@@ -40,7 +40,7 @@ vidFrames = read(readerobj);
 numFrames=size(vidFrames,4);
 videoFrame      = vidFrames(:,:,:,1);
 bbox = step(faceDetector, videoFrame);
-            box_range=[bbox(2)-40,bbox(2)+bbox(4)+40,bbox(1)-50,bbox(1)+bbox(3)+50];
+            box_range=[bbox(2)-80,bbox(2)+bbox(4)+80,bbox(1)-100,bbox(1)+bbox(3)+100];
             if(box_range(1)<=0) box_range(1)=0;end
             if(box_range(2)>size(videoFrame,1)) box_range(2)=size(videoFrame,1);end
             if(box_range(3)<=0) box_range(3)=0;end
@@ -48,12 +48,13 @@ bbox = step(faceDetector, videoFrame);
 % Create a MATLAB movie struct from the video frames.
 for k = 1 : numFrames
     mov(k).cdata = imresize(vidFrames(box_range(1):box_range(2),box_range(3):box_range(4),:,k),3);
-    mov(k).colormap = [];
+%     mov(k).colormap = [];
 end
 hf = figure; 
 % Resize figure based on the video's width and height
+imagesc(mov(1).cdata);
 set(hf, 'position', [2250 -100 size(mov(1).cdata,1) size(mov(1).cdata,2)])
-movie(hf, mov, [1 1 2 3], readerobj.FrameRate); 
+
 fprintf('press any key to continue..')
 pause
 close(hf)
@@ -65,7 +66,7 @@ release(faceDetector)
 pause(10)
 
 tic
-for s_ind=9:34
+for s_ind=12:17
     
     %if recording videos, skip speaker Nr.21 coz theres no video for this
     %speaker
@@ -94,15 +95,15 @@ for s_ind=9:34
             videoFrame      = vidFrames(:,:,:,1);
             bbox = step(faceDetector, videoFrame);
             if(isempty(bbox)) continue; end
-            box_range=[bbox(2)-40,bbox(2)+bbox(4)+40,bbox(1)-50,bbox(1)+bbox(3)+50];
-            if(box_range(1)<=1) box_range(1)=1;end
+            box_range=[bbox(2)-80,bbox(2)+bbox(4)+80,bbox(1)-100,bbox(1)+bbox(3)+100];
+            if(box_range(1)<=0) box_range(1)=0;end
             if(box_range(2)>size(videoFrame,1)) box_range(2)=size(videoFrame,1);end
-            if(box_range(3)<=1) box_range(3)=1;end
+            if(box_range(3)<=0) box_range(3)=0;end
             if(box_range(4)>size(videoFrame,2)) box_range(4)=size(videoFrame,2);end
            % Create a MATLAB movie struct from the video frames.
            for k = 1 : numFrames
                  mov(k).cdata = imresize(vidFrames(box_range(1):box_range(2),box_range(3):box_range(4),:,k),3);
-                 mov(k).colormap = [];
+                 %mov(k).colormap = [];
            end
            
             % Create a cascade detector object.
@@ -117,32 +118,41 @@ for s_ind=9:34
            hf = figure; 
 
            % Resize figure based on the video's width and height
-           set(hf, 'position', [2250 -100 size(mov(1).cdata,1) size(mov(1).cdata,2)])
-           movie(hf, mov, [1 1 2 3], readerobj.FrameRate); 
-           %start recording
+           
+           %movie(hf, mov, [1 1 2], readerobj.FrameRate); 
+            numFrames = size(vidFrames,4);
+            imagesc(mov(1).cdata);
+            set(hf, 'position', [2250 -100 size(mov(1).cdata,1) size(mov(1).cdata,2)])
+            %start recording
             %if only 1 sensor:
-            %commandJAER(u2,'zerotimestamps')
-            pause(0.5)
-           %commandJAER(u2,['startlogging ' filename])
             
-            %commandJAER(handles.u1,['startlogging ' filename '\cochlea'])
+            pause(0.2)
+            commandJAER(u2,'zerotimestamps')
+           commandJAER(u2,['startlogging ' filename])
+            pause(0.5)
+    %commandJAER(handles.u1,['startlogging ' filename '\cochlea'])
     %         commandJAER(handles.u1,'zerotimestamps')
     %         commandJAER(handles.u1,'togglesynclogging')
-           
-           % Playback movie once at the video's frame rate
-
-           movie(hf, mov, 1, readerobj.FrameRate);           
-            %commandJAER(u2,['stoplogging ' filename '\DVS'])
-            pause(0.2)
-               %         commandJAER(handles.u1,'togglesynclogging')
+       
+            for i=1:numFrames
+                tic
+                imagesc(mov(i).cdata);
+                %set(hf, 'position', [2250 -100 size(mov(1).cdata,1) size(mov(1).cdata,2)])
+                pause(1/readerobj.FrameRate-toc);
+            end
+            
+           % Playback movie once at the video's frame rate  
+            commandJAER(u2,['stoplogging ' filename '\DVS'])
+            pause(0.5)
+    %         commandJAER(handles.u1,'togglesynclogging')
     %         movefile([default_jpath 'Cochlea*'], [filename]);
     %         movefile([default_jpath 'DAVIS240C*'], [filename]);
     %         movefile([default_jpath 'JAERViewer*'], filename)
            close(hf)
            delete(readerobj)
            clear('readerobj')
-           clear('mov')
             release(faceDetector)
+            rec_cnt=rec_cnt+1
 
 
         end
